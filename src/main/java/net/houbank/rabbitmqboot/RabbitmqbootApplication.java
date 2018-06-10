27b68d96c1279f3ce71cede9af05105dd1290ed0
Zhang Yue <zhangyue01@houbank.cn>
2018-06-10 19:11:22 +0800

    Initial commit

---
+++ src/main/java/net/houbank/rabbitmqboot/RabbitmqbootApplication.java
@@ -0,0 +1,12 @@
+package net.houbank.rabbitmqboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqbootApplication.class, args);
+	}
+}
---
+++ src/test/java/net/houbank/rabbitmqboot/RabbitmqbootApplicationTests.java
@@ -0,0 +1,16 @@
+package net.houbank.rabbitmqboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
